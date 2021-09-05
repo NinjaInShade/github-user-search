989a9bfdddc023dd432c5e827eb4b28794185252
Leon Michalak <leonmichalak6@gmail.com>
2021-09-05 23:53:30 +0100

    LocalStorage persistant theme added

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const body = document.body;
+const ThemeSwitchTxt = document.getElementById('theme-switch-text');
+const moonIcon = document.getElementById('moon');
+const sunIcon = document.getElementById('sun');
+
 // Fetch user by username with github API
 function fetchUser(username) {
   return fetch(`https://api.github.com/users/${username}`)
@@ -17,32 +22,50 @@ function updateDOM(user) {
   console.log(user);
 }
 
-// Switch light themes
-function switchTheme() {
-  const body = document.body;
-  const ThemeSwitchTxt = document.getElementById('theme-switch-text');
-  const moonIcon = document.getElementById('moon');
-  const sunIcon = document.getElementById('sun');
-
-  if (body.classList.contains('dark-theme')) {
-    ThemeSwitchTxt.innerText = 'Dark';
-  } else {
-    ThemeSwitchTxt.innerText = 'Light';
-  }
-
+function updateThemeClasses() {
   moonIcon.classList.toggle('hidden');
   sunIcon.classList.toggle('hidden');
   body.classList.toggle('dark-theme');
 }
 
+// Switch light themes
+function switchTheme() {
+  // If it contains dark-theme class, we're switching to light theme
+  if (body.classList.contains('dark-theme')) {
+    ThemeSwitchTxt.innerText = 'Dark';
+
+    localStorage.setItem('theme', 'light');
+  } else {
+    ThemeSwitchTxt.innerText = 'Light';
+
+    localStorage.setItem('theme', 'dark');
+  }
+
+  return updateThemeClasses();
+}
+
+function initTheme() {
+  // If user has dark preference, set the dark theme by default.
+  // LocalStorage overrides this however, as the user has then changed the theme,
+  // which we want to persist to those settings then.
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+  const storedTheme = localStorage.getItem('theme');
+
+  if (storedTheme === 'dark') {
+    ThemeSwitchTxt.innerText = 'Light';
+
+    return updateThemeClasses();
+  }
+
+  if (prefersDarkScheme.matches) {
+    ThemeSwitchTxt.innerText = 'Dark';
+
+    return updateThemeClasses();
+  }
+}
+
 // Default user to be displayed
 fetchUser('NinjaInShade');
 
-// If user has dark preference, set the dark theme by default.
-const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-
-if (prefersDarkScheme.matches) {
-  document.body.classList.add('dark-theme');
-} else {
-  document.body.classList.remove('dark-theme');
-}
+// Default theme
+initTheme();
